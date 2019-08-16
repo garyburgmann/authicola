e2e3da1748c5b6244ccbade2d9f8c56c3631a0a4
Gary Burgmann <garyburgmann@gmail.com>
2019-08-16 12:09:55 +1000

    updated setup.py with long_description

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-import os
 from setuptools import setup
+from os import path
 
 import authicola as meta
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name=meta.__name__,
@@ -12,6 +17,8 @@ setup(
     ],
     include_package_data=True,
     description=meta.__description__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author=meta.__author__,
     author_email=meta.__author_email__,
     url=meta.__url__,
